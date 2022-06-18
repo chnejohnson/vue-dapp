92931a80cf0d7ce9116ce59d921c95f48df2b8c8
Renato Cardoso <re2005@gmail.com>
2022-06-18 16:25:41 +0200

    Update useEthers.ts

--- src/composables/useEthers.ts
+++ src/composables/useEthers.ts
@@ -6,7 +6,7 @@ import {
 } from '@ethersproject/providers'
 import { BigNumber, Signer } from 'ethers'
 import { NETWORK_DETAILS } from '../constants'
-import { AddEthereumChainParameter } from 'vue-dapp/wallets'
+import { AddEthereumChainParameter } from '../wallets'
 
 export type { Web3Provider, Signer, Network }
 
